456e768a3bd800d61e1db53c74d8ed3bdc2c8e85
Pawel Sieniarski <paabson@gmail.com>
2015-06-17 23:03:19 +0200

    added helpers

--- spec/remap.spec.js
+++ spec/remap.spec.js
@@ -9,15 +9,17 @@ var sinon = require('sinon');
 // src code
 var remap = require('../src/remap.js');
 
+// helpers
+var testString  = '<div class="foo bar"></div><div id="unique"></div>';
+var testPattern = /(class|id)[ \t]*=[ \t]*"([^"]+)"/gm;  
+
+// tests
 describe('eachMatch', function() {
-	it('wywolanie zwrotne wyzwalane jest dla kazdego dopasowania', function() {
+	it('callback should be involved for each match', function() {
 		var callback = sinon.spy();
 
 		remap.eachMatch('example string example', /example/gm, callback); // should be called twice 
 		assert.isTrue(callback.calledTwice);
 	});
-
-
-	
 });
 
