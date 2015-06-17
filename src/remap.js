9d3a09000e4d4b8f51c1ab66c33eaac80d5f7573
Pawel Sieniarski <paabson@gmail.com>
2015-06-17 23:33:13 +0200

    added extractSelectors function

--- src/remap.js
+++ src/remap.js
@@ -10,11 +10,35 @@ var pattern = CONFIG.classID;
 function eachMatch(str, pattern, callback) {
 	var match; 
 	while(match = pattern.exec(str)) {
-		callback(match)
+		callback(match);
 	}
 }
 
+function extractSelectors(str, pattern) {
+	var result = { ids: {}, classes: {} };
+
+	eachMatch(str, pattern, function(match) {
+		var type = match[1];
+		var selectors = match[2].split(' ');
+		
+		if(type == 'class') {
+			selectors.forEach(function(selector) {
+				result.classes[selector] = '';
+			});
+
+		} else if(type == 'id') {
+			selectors.forEach(function(selector) {
+				result.ids[selector] = '';
+			});
+		}
+	});
+
+	return result;
+}
+
 module.exports = {
-	eachMatch: eachMatch
+	eachMatch: eachMatch,
+	extractSelectors: extractSelectors
+
 }
 
